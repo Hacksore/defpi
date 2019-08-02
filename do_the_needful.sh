bc92376278c90703d696b2f12c37345fe8b68581
Sean Boult <hacksore@mcskinsearch.com>
2019-08-01 20:26:27 -0500

    Update setup script

--- do_the_needful.sh
+++ do_the_needful.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+RUNNING_ON_PI=$(uname -a | grep "raspberrypi")
+
 # this script should do everything to setup the pi 
 sudo apt -y update
 
-# Raspbian has this but stock debian does not have this installed so we do this
-sudo apt -y install chromium-browser
+# Raspbian has these but stock debian does not have this installed so we do this
+if [ ! -z $RUNNING_ON_PI ]; then
+  sudo apt -y install chromium-browser git curl
+fi
 
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt install -y nodejs
+curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+sudo apt install -y nodejs npm
 
 npm install -g forever
 
-# install screen
-git clone git@github.com:goodtft/LCD-show.git /tmp/lcd
-
+# install LCD screen
+git clone https://github.com:goodtft/LCD-show /tmp/lcd
 chmod +x /tmp/lcd/LCD35-show
-sudo bash /tmp/lcd/LCD35-show
+
+# run the setup script
+sudo bash /tmp/lcd/LCD35-show 90
 
 sudo apt install -y hostapd
 sudo apt install -y dnsmasq
@@ -24,4 +29,8 @@ sudo systemctl stop hostapd
 sudo systemctl stop dnsmasq
 
 # setup auto start
-cat "config/autostart" > "/etc/xdg/lxsession/LXDE-pi/autostart"
+if [ ! -z $RUNNING_ON_PI ]; then
+  cat "config/autostart" > "/etc/xdg/lxsession/LXDE-pi/autostart"
+else
+  cat "config/autostart" > "/etc/xdg/lxsession/LXDE/autostart"
+fi
