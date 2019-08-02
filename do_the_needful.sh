6e709db62f60f7e48a29fb55870d151858abf674
Sean Boult <hacksore@mcskinsearch.com>
2019-08-01 20:39:37 -0500

    Add debug

--- do_the_needful.sh
+++ do_the_needful.sh
@@ -2,34 +2,41 @@
 
 RUNNING_ON_PI=$(uname -a | grep "raspberrypi")
 
-# this script should do everything to setup the pi 
-sudo apt -y update
-
-# Raspbian has these but stock debian does not have this installed so we do this
-if [ ! -z $RUNNING_ON_PI ]; then
-  sudo apt -y install chromium-browser git curl
+if [ ! -z "$RUNNING_ON_PI" ]; then
+  echo "Running on debian"
+else 
+  echo "Running on raspbian"
 fi
 
-curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
-sudo apt install -y nodejs npm
+# this script should do everything to setup the pi 
+sudo apt -y update > /dev/null
+
+# Raspbian has these but stock debian does not have this installed so we do this
+if [ ! -z "$RUNNING_ON_PI" ]; then
+  sudo apt -y install chromium git curl > /dev/null
+fi
+
+curl -sL https://deb.nodesource.com/setup_12.x | sudo bash - > /dev/null
+sudo apt install -y nodejs npm > /dev/null
 
 npm install -g forever
 
 # install LCD screen
-git clone https://github.com/goodtft/LCD-show /tmp/lcd
-chmod +x /tmp/lcd/LCD35-show
+if [ ! -z "$RUNNING_ON_PI" ]; then
+  git clone https://github.com/goodtft/LCD-show /tmp/lcd
+  chmod +x /tmp/lcd/LCD35-show
 
-# run the setup script
-sudo bash /tmp/lcd/LCD35-show 90
+  # run the setup script
+  sudo bash /tmp/lcd/LCD35-show 90
+fi
 
-sudo apt install -y hostapd
-sudo apt install -y dnsmasq
+sudo apt install -y hostapd dnsmasq > /dev/null
 
 sudo systemctl stop hostapd
 sudo systemctl stop dnsmasq
 
 # setup auto start
-if [ ! -z $RUNNING_ON_PI ]; then
+if [ ! -z "$RUNNING_ON_PI" ]; then
   cat "config/autostart" > "/etc/xdg/lxsession/LXDE-pi/autostart"
 else
   cat "config/autostart" > "/etc/xdg/lxsession/LXDE/autostart"
