f44c3286a852e7a59768054e22ffb229f4bb6f86
Sean Boult <hacksore@mcskinsearch.com>
2019-08-03 21:31:56 -0500

    Move env var export up

--- config/startup.sh
+++ config/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-forever start /home/pi/defpi/server/main.js
 export NODE_ENV="production"
+forever start /home/pi/defpi/server/main.js
 DISPLAY=:0 chromium-browser --kiosk --incognito http://localhost:1337
